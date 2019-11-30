d7582fee9dd6aed85f52012e5389272e1ceba86a
Tulio <tuliojardim@visaojr.com.br>
2019-11-30 16:23:02 -0300

    A few classes

---
+++ Codes/004-php/03-laravel/academico/database/factories/CidadesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Cidades;
+use Faker\Generator as Faker;
+
+$factory->define(Cidades::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ Codes/004-php/03-laravel/academico/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/welcome', 'PaginasController@welcome');
+
+Route::get('/', 'PaginasController@index');
+
+Route::get('/listar', 'PaginasController@listar');
+
+Route::get('/estados', 'EstadoController@index');
+
+Route::get('/cidades', 'CidadesController@index');
