d565f4e8ecf024d7e5a26b70b391bad5cb58bed0
Tulio <tuliojardim@visaojr.com.br>
2019-12-03 18:57:28 -0300

    Começo AtvPrat02

---
+++ AtvPrat02/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware' => ['auth']],function(){
+    
+    Route::group(['middleware' => ['admin']], function(){
+        Route::get('/admin', function () {
+            return view('welcome');
+        });
+    });
+
+    // Route::get('/usuario/index','AreaUsuarioController@index')->name('usuario.index');
+    Route::get('/home', function () {
+        return view('home');
+    })->name('home')->middleware('auth');
+
+});
+
+Auth::routes();
+
+// Route::get('/home', function() {
+//     return view('home');
+// })->name('home')->middleware('auth');
